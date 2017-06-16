bd8d962a8323f5a9c429678608e9a884c97200f4
HamzaAnis <hamzaanis9514@gmail.com>
2017-06-16 10:33:18 -0700

    Adding comments

--- rw.c
+++ rw.c
@@ -25,7 +25,9 @@ void initialize_readers_writer()
      */
     init_resource(&data, "Reader Writer");
     //  print_stats(&data);
+    //initialzing the mutex for reading
     sem_init(&mutexRead, 0, 1);
+    //initialzing the mutex for writing
     sem_init(&mutexWrite, 0, 1);
 }
 
@@ -33,24 +35,32 @@ void rw_read(char *value, int len)
 {
     // printf("NOTHING IMPLEMENTED YET FOR rw_read\n");
     // printf("Reader called\n");
+    //taking the lock
     sem_wait(&mutexRead);
+    //If only one is reading
     if (data.num_reads == 1)
     {
         // printf("The read mutex is set\n");
+        //take the lock for the writing
         sem_wait(&mutexWrite);
     }
+    //realease the lock
     sem_post(&mutexRead);
 
+    //read the resource
     read_resource(&data, value, len);
 
+    //take the lock
     sem_wait(&mutexRead);
     data.num_reads -= 1;
     // printf("The num_reads are %d\n",data.num_reads);
     if (data.num_reads == 0)
     {
+        //releasing the write lock
         sem_post(&mutexWrite);
         // printf("the Db is released\n");
     }
+    //releasing the read lock
     sem_post(&mutexRead);
     //non critical region
 }
@@ -58,8 +68,10 @@ void rw_read(char *value, int len)
 void rw_write(char *value, int len)
 {
     // printf("Write is called\n");
+    //taking lock for write
     sem_wait(&mutexWrite);
     write_resource(&data, value, len);
     // printf("Written");
+    //Releasing the lock for write
     sem_post(&mutexWrite);
 }
